ae8c0d831e0efc6ce96a3b728cf6cc28d5802873
ohyicong <36614573+ohyicong@users.noreply.github.com>
2020-07-18 17:54:58 +0800

    updated settings

--- main.py
+++ main.py
@@ -10,6 +10,8 @@ import os
 
 webdriver_path = os.getcwd()+"\\webdriver\\chromedriver.exe"
 image_path = os.getcwd()+"\\photos"
-image_scrapper = GoogleImageScraper(webdriver_path,image_path,"cat",10)
+search_key= "cat"
+number_of_images = 10 
+image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images)
 image_urls = image_scrapper.find_image_urls()
 image_scrapper.save_images(image_urls)
