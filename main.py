809d7f37f736333a63f116005a518bd969f8bc89
ohyicong <ohyicong@hotmail.com>
2020-08-24 20:13:56 +0800

    allow for multiple search keys

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ import os
 
 webdriver_path = os.getcwd()+"\\webdriver\\chromedriver.exe"
 image_path = os.getcwd()+"\\photos"
-search_key= "cat"
+#add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
+search_keys= ["cat","t-shirt"]
 number_of_images = 20
 headless = False
 #min_resolution = (width,height)
 min_resolution=(0,0)
 #max_resolution = (width,height)
 max_resolution=(1920,1080)
-image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
-image_urls = image_scrapper.find_image_urls()
-image_scrapper.save_images(image_urls)
+for search_key in search_keys:
+    image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
+    image_urls = image_scrapper.find_image_urls()
+    image_scrapper.save_images(image_urls)
