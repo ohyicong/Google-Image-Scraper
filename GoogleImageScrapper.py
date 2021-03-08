e7aa67b95f50a1d99061dc7eeffc6114010976ab
Oh Yicong <ohyicong@users.noreply.github.com>
2021-03-08 20:45:28 +0800

    Optimised Code

--- GoogleImageScrapper.py
+++ GoogleImageScrapper.py
@@ -7,6 +7,9 @@ Created on Sat Jul 18 13:01:02 2020
 #import selenium drivers
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 #import helper libraries
 import time
@@ -16,7 +19,7 @@ import os
 import requests
 import struct
 import imghdr
-
+from PIL import Image
 
 class GoogleImageScraper():
     def __init__(self,webdriver_path,image_path, search_key="cat",number_of_images=1,headless=False,min_resolution=(0,0),max_resolution=(1920,1080)):
@@ -35,6 +38,9 @@ class GoogleImageScraper():
         self.headless=headless
         self.min_resolution = min_resolution
         self.max_resolution = max_resolution
+        self.saved_extension = "jpg"
+        self.valid_extensions = ["jpg","png","jpeg"]
+        
     def find_image_urls(self):
         """
             This function search and return a list of image urls based on the search key.
@@ -63,25 +69,22 @@ class GoogleImageScraper():
                 imgurl.click()
                 
                 #select image from the popup
-                time.sleep(3)
+                time.sleep(1)
+                WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "n3VNCb")))
                 images = driver.find_elements_by_class_name("n3VNCb")
+                
                 for image in images:
-                    
-                    #only download images that ends with jpg/png/jpeg extensions
-                   
-                    if (image.get_attribute("src")[-3:].lower() in ["jpg","png","jpeg"]):
+                    #only download images that starts with http
+                    if(image.get_attribute("src")[:4].lower() in ["http"]):
                         print("%d. %s"%(count,image.get_attribute("src")))
                         image_urls.append(image.get_attribute("src"))
                         count +=1
                         break
-                        
                 #scroll page to load next image
                 driver.execute_script("window.scrollTo(0, "+str(indx*150)+");")
-                time.sleep(3)
+                time.sleep(1)
             except Exception as e:
-                print("GoogleImageScraper Skip: Unable to get the link for this photo")
-             
-        
+                print("GoogleImageScraper Skip: Unable to get the link for this photo",e)
         driver.close()
         return image_urls
     
@@ -98,57 +101,21 @@ class GoogleImageScraper():
         print("GoogleImageScraper Notification: Saving Image... Please wait.")
         for indx,image_url in enumerate(image_urls):
             try:
-                
-                filename = self.search_key+str(indx)+'.jpg'
+                filename = "%s%s.%s"%(self.search_key,str(indx),self.saved_extension)
                 image_path = os.path.join(self.image_path, filename)
                 print("%d .Image saved at: %s"%(indx,image_path))
                 image = requests.get(image_url)
                 if image.status_code == 200:
                     with open(image_path, 'wb') as f:
                         f.write(image.content)
-                    image_resolution = self.get_image_size(image_path)
-                   
+                    image_from_web = Image.open(image_path)
+                    image_resolution = image_from_web.size
                     if image_resolution != None:
                         if image_resolution[0]<self.min_resolution[0] or image_resolution[1]<self.min_resolution[1] or image_resolution[0]>self.max_resolution[0] or image_resolution[1]>self.max_resolution[1]:
                             #print("GoogleImageScraper Notification: %s did not meet resolution requirements."%(image_url))
                             os.remove(image_path)
-                 
-
-            except Exception:
+            except Exception as e:
                 #print("GoogleImageScraper Error: Failed to be downloaded.")
                 pass
         print("GoogleImageScraper Notification: Download Completed.")
-    
-    def get_image_size(self,fname):
-        with open(fname, 'rb') as fhandle:
-            head = fhandle.read(24)
-            if len(head) != 24:
-                return None
-            what = imghdr.what(None, head)
-            if what == 'png':
-                check = struct.unpack('>i', head[4:8])[0]
-                if check != 0x0d0a1a0a:
-                    return None
-                width, height = struct.unpack('>ii', head[16:24])
-            elif what == 'gif':
-                width, height = struct.unpack('<HH', head[6:10])
-            elif what == 'jpeg':
-                try:
-                    fhandle.seek(0) # Read 0xff next
-                    size = 2
-                    ftype = 0
-                    while not 0xc0 <= ftype <= 0xcf or ftype in (0xc4, 0xc8, 0xcc):
-                        fhandle.seek(size, 1)
-                        byte = fhandle.read(1)
-                        while ord(byte) == 0xff:
-                            byte = fhandle.read(1)
-                        ftype = ord(byte)
-                        size = struct.unpack('>H', fhandle.read(2))[0] - 2
-                    # We are at a SOFn block
-                    fhandle.seek(1, 1)  # Skip `precision' byte.
-                    height, width = struct.unpack('>HH', fhandle.read(4))
-                except Exception: #IGNORE:W0703
-                    return None
-            else:
-                return None
-            return width, height
+        
--- main.py
+++ main.py
@@ -13,11 +13,11 @@ image_path = os.getcwd()+"\\photos"
 #add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
 search_keys= ["cat","t-shirt"]
 number_of_images = 10
-headless = False
+headless = True
 #min_resolution = (width,height)
 min_resolution=(0,0)
 #max_resolution = (width,height)
-max_resolution=(1920,1080)
+max_resolution=(192,108)
 for search_key in search_keys:
     image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
     image_urls = image_scrapper.find_image_urls()
