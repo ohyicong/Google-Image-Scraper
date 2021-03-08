1edef0d2674de3c5fc345f87b12e8f6aed21e628
Oh Yicong <ohyicong@users.noreply.github.com>
2021-03-08 20:56:55 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ Created on Sun Jul 12 11:02:06 2020
 @author: OHyic
 
 """
+#Import libraries
 from GoogleImageScrapper import GoogleImageScraper
 import os
 
+#Define file path
 webdriver_path = os.getcwd()+"\\webdriver\\chromedriver.exe"
 image_path = os.getcwd()+"\\photos"
-#add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
+
+#Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
 search_keys= ["cat","t-shirt"]
+
+#Parameters
 number_of_images = 10
 headless = True
-#min_resolution = (width,height)
 min_resolution=(0,0)
-#max_resolution = (width,height)
-max_resolution=(192,108)
+max_resolution=(1920,1080)
+
+#Main program
 for search_key in search_keys:
     image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
     image_urls = image_scrapper.find_image_urls()
