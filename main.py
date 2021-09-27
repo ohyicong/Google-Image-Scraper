34c587407c1753c58659c86d7a59afefd96637ed
ohyicong <ohyicong@hotmail.com>
2021-09-27 23:16:25 +0800

    Prettify code

--- main.py
+++ main.py
@@ -9,24 +9,26 @@ Created on Sun Jul 12 11:02:06 2020
 from GoogleImageScrapper import GoogleImageScraper
 import os
 
-#Define file path
-webdriver_path = os.path.normpath(os.getcwd()+"\\webdriver\\chromedriver.exe")
-image_path = os.path.normpath(os.getcwd()+"\\photos")
 
-#Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-search_keys= ['apple','t-shirt']
+if __name__ == "__main__":
+    #Define file path
+    webdriver_path = os.path.normpath(os.getcwd()+"\\webdriver\\chromedriver.exe")
+    image_path = os.path.normpath(os.getcwd()+"\\photos")
 
-#Parameters
-number_of_images = 100
-headless = False
-min_resolution=(0,0)
-max_resolution=(9999,9999)
+    #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
+    search_keys= ['apple','t-shirt']
 
-#Main program
-for search_key in search_keys:
-    image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
-    image_urls = image_scrapper.find_image_urls()
-    image_scrapper.save_images(image_urls)
+    #Parameters
+    number_of_images = 100
+    headless = False
+    min_resolution=(0,0)
+    max_resolution=(9999,9999)
 
-#Release resources    
-del image_scrapper
+    #Main program
+    for search_key in search_keys:
+        image_scrapper = GoogleImageScraper(webdriver_path,image_path,search_key,number_of_images,headless,min_resolution,max_resolution)
+        image_urls = image_scrapper.find_image_urls()
+        image_scrapper.save_images(image_urls)
+    
+    #Release resources    
+    del image_scrapper
