79f384259cb1eebc69142c3949c5c8efe95fc0f1
ItsGoldeneyes <97778776+ItsGoldeneyes@users.noreply.github.com>
2022-06-23 02:00:55 -0400

    Resolving issue #24 + misspelling scraper

--- main.py
+++ main.py
@@ -8,18 +8,18 @@ Created on Sun Jul 12 11:02:06 2020
 #Import libraries
 import os
 import concurrent.futures
-from GoogleImageScrapper import GoogleImageScraper
+from GoogleImageScraper import GoogleImageScraper
 from patch import webdriver_executable
 
 
 def worker_thread(search_key):
-    image_scrapper = GoogleImageScraper(
+    image_scraper = GoogleImageScraper(
         webdriver_path, image_path, search_key, number_of_images, headless, min_resolution, max_resolution)
-    image_urls = image_scrapper.find_image_urls()
-    image_scrapper.save_images(image_urls)
+    image_urls = image_scraper.find_image_urls()
+    image_scraper.save_images(image_urls)
 
     #Release resources
-    del image_scrapper
+    del image_scraper
 
 if __name__ == "__main__":
     #Define file path
@@ -27,15 +27,15 @@ if __name__ == "__main__":
     image_path = os.path.normpath(os.path.join(os.getcwd(), 'photos'))
 
     #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-    search_keys = ['apple', 't-shirt']
+    search_keys = ['t-shirt', "long sleeve shirts", "shorts", "pants"]
 
     #Parameters
-    number_of_images = 10               # Desired number of images
+    number_of_images = 100               # Desired number of images
     headless = True                    # True = No Chrome GUI
     min_resolution = (0, 0)            # Minimum desired image resolution
-    max_resolution = (9999, 9999)      # Maximum desired image resolution
+    max_resolution = (2000, 2000)      # Maximum desired image resolution
     max_missed = 10                    # Max number of failed images before exit
-    number_of_workers = 1              # Number of "workers" used
+    number_of_workers = 4              # Number of "workers" used
 
     #Run each search_key in a separate thread
     #Automatically waits for all threads to finish
