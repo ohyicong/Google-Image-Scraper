3cf11852550992e8a76736e299438706c9b52798
AoverK <AdrianLKennedy@gmail.com>
2022-12-11 20:13:45 -0500

    Upgraded to support Google Search Operators
    - Separated Search Keys and Path Keys parameters so folder names can be different from search parameters
    
    View examples of search parameters at the links below.
    
    https://ahrefs.com/blog/google-advanced-search-operators/
    
    https://searchengineland.com/advanced-google-search-operators-388355

--- main.py
+++ main.py
@@ -12,9 +12,9 @@ from GoogleImageScraper import GoogleImageScraper
 from patch import webdriver_executable
 
 
-def worker_thread(search_key):
+def worker_thread(search_key, path_key):
     image_scraper = GoogleImageScraper(
-        webdriver_path, image_path, search_key, number_of_images, headless, min_resolution, max_resolution)
+        webdriver_path, image_path, search_key, path_key, number_of_images, headless, min_resolution, max_resolution)
     image_urls = image_scraper.find_image_urls()
     image_scraper.save_images(image_urls, keep_filenames)
 
@@ -27,10 +27,11 @@ if __name__ == "__main__":
     image_path = os.path.normpath(os.path.join(os.getcwd(), 'photos'))
 
     #Add new search key into array ["cat","t-shirt","apple","orange","pear","fish"]
-    search_keys = list(set(["cat", "t-shirt"]))
+    search_keys = list(set(['business quotes site:pinterest.com']))
+    path_keys = list(set(['business quotes pinterest']))
 
     #Parameters
-    number_of_images = 5                # Desired number of images
+    number_of_images = 100                # Desired number of images
     headless = True                     # True = No Chrome GUI
     min_resolution = (0, 0)             # Minimum desired image resolution
     max_resolution = (9999, 9999)       # Maximum desired image resolution
@@ -42,4 +43,4 @@ if __name__ == "__main__":
     #Automatically waits for all threads to finish
     #Removes duplicate strings from search_keys
     with concurrent.futures.ThreadPoolExecutor(max_workers=number_of_workers) as executor:
-        executor.map(worker_thread, search_keys)
+        executor.map(worker_thread, search_keys, path_keys)
